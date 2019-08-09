ded1a7c435dff2ba952f6eb35ff6456fdddb02a1
Jiabo.Wu <wujbow@cn.ibm.com>
2019-08-09 09:27:07 +0800

    init

--- src/main/java/com/wujiabo/fsd/domain/auth/User.java
+++ src/main/java/com/wujiabo/fsd/domain/auth/User.java
@@ -6,10 +6,10 @@ import javax.validation.constraints.Size;
 
 public class User {
     @ApiModelProperty(value = "用户名", required = true)
-    @Size(min=6, max=20)
+    @Size(min=3, max=20)
     private String name;
     @ApiModelProperty(value = "密码", required = true)
-    @Size(min=8, max=20)
+    @Size(min=6, max=20)
     private String password;
 
     public String getName() {
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,8 @@
-INSERT INTO sys_role (id, nameZh, name) VALUES (1,'用户','ROLE_USER');
-INSERT INTO sys_role (id, nameZh, name) VALUES (2,'管理员','ROLE_ADMIN');
+INSERT INTO sys_role (id, nameZh, name) VALUES (1,'管理员','ROLE_ADMIN');
+INSERT INTO sys_role (id, nameZh, name) VALUES (2,'用户','ROLE_USER');
+
+INSERT INTO sys_user (id, name, nickname, password) VALUES (1,'admin','管理员账号','123456');
+INSERT INTO sys_user (id, name, nickname, password) VALUES (2,'les123','用户账号','123456');
+
+INSERT INTO sys_user_role (id, user_id, role_id) VALUES (1,1,1);
+INSERT INTO sys_user_role (id, user_id, role_id) VALUES (2,2,2);
