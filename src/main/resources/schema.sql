0c17ee52c7946ef15b63dc922dcf8ae0fa00be32
Jiabo.Wu <wujbow@cn.ibm.com>
2019-08-13 15:23:32 +0800

    init

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,18 +1,17 @@
 CREATE TABLE sys_user (
-  id int(11) NOT NULL,
+  id int(11) NOT NULL auto_increment,
   name varchar(11) NOT NULL,
-  nickname varchar(11) NOT NULL,
   password varchar(64) NOT NULL,
   PRIMARY KEY (id)
 );
 CREATE TABLE sys_role (
-  id int(11) NOT NULL,
+  id int(11) NOT NULL auto_increment,
   nameZh varchar(11) NOT NULL,
   name varchar(11),
   PRIMARY KEY (id)
 );
 CREATE TABLE sys_user_role (
-  id int(11) NOT NULL,
+  id int(11) NOT NULL auto_increment,
   user_id int(11),
   role_id int(11),
   PRIMARY KEY (id)
